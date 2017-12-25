e304a01370d246173cd0a4ccc981a19d312d7b43
Klepnev <klepnev-ea@yandex.ru>
2017-12-25 10:38:59 +0300

    опросник

--- source/js/app.js
+++ source/js/app.js
@@ -281,4 +281,92 @@ $(document).ready(function() {
 			$(this).attr('placeholder',placeholderVal);
 		}
 	});
+
+
+	// Select
+	$('.select').each(function(){
+		// Variables
+		var $this = $(this),
+			selectOption = $this.find('option'),
+			selectOptionLength = selectOption.length,
+			selectedOption = selectOption.filter(':selected'),
+			dur = 500;
+
+		$this.hide();
+		// Wrap all in select box
+		$this.wrap('<div class="select"></div>');
+		// Style box
+		$('<div>',{
+			class: 'select__gap',
+			text: 'Задать вопрос по дисциплине:'
+		}).insertAfter($this);
+		
+		var selectGap = $this.next('.select__gap'),
+			caret = selectGap.find('.caret');
+		// Add ul list
+		$('<ul>',{
+			class: 'select__list'
+		}).insertAfter(selectGap);		
+
+		var selectList = selectGap.next('.select__list');
+		// Add li - option items
+		for(var i = 0; i < selectOptionLength; i++){
+			$('<li>',{
+				class: 'select__item',
+				html: $('<span>',{
+					text: selectOption.eq(i).text()
+				})				
+			})
+			.attr('data-value', selectOption.eq(i).val())
+			.appendTo(selectList);
+		}
+		// Find all items
+		var selectItem = selectList.find('li');
+
+		selectList.slideUp(0);
+		selectGap.on('click', function(){
+			if(!$(this).hasClass('on')){
+				$(this).addClass('on');
+				selectList.slideDown(dur);
+
+				selectItem.on('click', function(){
+					var chooseItem = $(this).data('value');
+
+					$('select').val(chooseItem).attr('selected', 'selected');
+					selectGap.text($(this).find('span').text());
+
+					selectList.slideUp(dur);
+					selectGap.removeClass('on');
+				});
+				
+			} else {
+				$(this).removeClass('on');
+				selectList.slideUp(dur);
+			}
+		});		
+
+	});
+
+
+	/*просто заглушки для демонстрации*/
+
+	setTimeout(function () {
+		$('.questions').addClass('active');
+	}, 2000);
+
+	$('.questions__close').on('click', function () {
+		$('.questions__select').addClass('active');
+		$('.questions__form').removeClass('active');
+		$('.questions__ansver').removeClass('active');
+	});
+
+	$('.select__item').on('click', function () {
+		$('.questions__form').addClass('active');
+	});
+
+	$('.questions__form-btn').on('click', function () {
+		$('.questions__select').removeClass('active');
+		$('.questions__form').removeClass('active');
+		$('.questions__ansver').addClass('active');
+	});
 });
